60115ae66756be36f2dc40eba8f7e15f82cf57b2
Sungguk Lim <limasdf@gmail.com>
2023-10-07 21:43:29 +0900

    rename package

---
+++ function.go
@@ -0,0 +1,22 @@
+package function
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("HelloWorld", helloWorld)
+}
+
+// helloWorld writes "Hello, World!" to the HTTP response.
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World!")
+}
+
+func run() {
+	koreainvestment.initia
+
+}
